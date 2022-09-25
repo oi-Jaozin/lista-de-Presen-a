6fe27dbc4f4a1bbfabb0ff9104b1e9558ea48a2b
João Pedro <joaopedro2210@gmail.com>
2022-09-25 16:26:06 -0300

    first commit

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export function Card({name, time}){
+    return(
+        <div className="Card">
+            <strong>{name}</strong>
+            <small>{time}</small>
+        </div>
+    )
+
+     
+}
